2d079b4233de677d6729b2d97b973834ba127eb1
lordksix <wladimir.pasquel@urp.edu.pe>
2023-04-23 18:39:49 -0500

    Update home app UI

---
+++ home/static/home/js/main.js
@@ -0,0 +1,27 @@
+const menuBurger = document.querySelector('.menu_hamburger');
+const menuBlock = document.querySelector('.menu');
+const menuOptions = document.querySelectorAll('.menu li');
+const bodyNode = document.querySelector('body');
+
+function menuModal() {
+  menuBurger.computedStyleMap.display = 'none';
+  menuBlock.classList.add('menu_popup');
+  bodyNode.classList.add('modal-active');
+}
+
+function menuInteraction() {
+  menuBlock.classList.add('out');
+  menuBurger.computedStyleMap.display = 'block';
+  bodyNode.classList.remove('modal-active');
+  setTimeout(() => menuBlock.classList.remove('menu_popup', 'out'), 500);
+}
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 768) {
+    menuBlock.classList.remove('menu_popup', 'out');
+    bodyNode.classList.remove('modal-active');
+  }
+});
+
+menuBurger.addEventListener('click', menuModal);
+menuOptions.forEach((option) => option.addEventListener('click', menuInteraction));
