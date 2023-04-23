0ebaa17798f34e8240207ce9cf611cef133e682e
lordksix <wladimir.pasquel@urp.edu.pe>
2023-04-23 16:13:57 -0500

    Update admin models

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,5 @@ from django.views.generic import TemplateView
 
 app_name = 'home'
 urlpatterns = [
-    #path('', TemplateView.as_view(template_name='home/main.html'),name="home"),
+    path('', TemplateView.as_view(template_name='home/main.html'),name="home"),
 ]
