77c52f740bb5e185eaf36f2ba018f2d3b823d757
lordksix <wladimir.pasquel@urp.edu.pe>
2023-04-23 00:41:54 -0500

    Setup initial app

--- producerApp/producerApp/urls.py
+++ producerApp/producerApp/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.views.generic import RedirectView
+
+#for development
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', include('home.urls')),
+    path('', RedirectView.as_view(url='home/')),
 ]
+#for development
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
