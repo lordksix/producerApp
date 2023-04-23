5db94b7e927db3901d2c0bbedd66a947057c5158
lordksix <wladimir.pasquel@urp.edu.pe>
2023-04-23 06:09:31 -0500

    Update settings.py

--- producerApp/producerApp/settings.py
+++ producerApp/producerApp/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') or config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default=os.environ.get('DJANGO_SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') or config('DEBUG', cast=bool)
+DEBUG = config('DEBUG', default=os.environ.get('DEBUG'))
 
 ALLOWED_HOSTS = ['*']
 
