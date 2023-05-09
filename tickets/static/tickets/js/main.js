62044241795d6660db206d92a4ac03dfbe01ccd0
lordksix <wladimir.pasquel@urp.edu.pe>
2023-05-09 07:26:24 -0500

    Update ticketsApp

--- tickets/static/tickets/js/main.js
+++ tickets/static/tickets/js/main.js
@@ -1,6 +1,7 @@
 import { menuPopUp, menuPopOut, menuModalResize } from './MenuModalModule.js';
 import { sliderEvents } from './sliderModule.js';
 import { renderEvents } from './eventGenerator.js';
+import { smoothScroll } from './ScrollEffects.js';
 
 const menuBurger = document.querySelector('.menu_hamburger');
 const menuBlock = document.querySelector('.menu');
@@ -10,10 +11,12 @@ const windowWidth = window.innerWidth;
 const menuLinks = document.querySelectorAll('.menu li a');
 const eventSlider = document.querySelectorAll('.event-slide');
 const eventSec = document.querySelector('#event-sec');
+let headerHeight = document.getElementById('header').offsetHeight * 0.6;
 
 const menuPop = () => menuPopUp(menuBurger, menuBlock, bodyNode);
 const menuVanish = () => menuPopOut(menuBurger, menuBlock, bodyNode);
 const menuResize = () => menuModalResize(menuBlock, bodyNode, windowWidth);
+const smoothScrollEvent = (e) => smoothScroll(e, headerHeight);
 const slider = () => sliderEvents('#slider-wrapper', 'next', 'prev', '.event-slide');
 const eventSecRender = (e) => {
   if(!eventSec.classList.contains('section')) eventSec.classList.add('section');
@@ -29,4 +32,7 @@ window.addEventListener('load', slider);
 
 menuBurger.addEventListener('click', menuPop);
 menuOptions.forEach((option) => option.addEventListener('click', menuVanish));
-eventSlider.forEach((eventSlide) => eventSlide.addEventListener('click', eventSecRender));
+eventSlider.forEach((eventSlide) => eventSlide.addEventListener('click', (e) => {
+  eventSecRender(e);
+  smoothScrollEvent(e);
+}));
